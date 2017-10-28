c83bacf073dd9b3ecc20dadea190c28efdcdd950
Prismaillya <31351878+Prisma-illya@users.noreply.github.com>
2017-10-28 22:25:41 +0800

    程序主函数类

---
+++ kirinos/class/kirinos.php
@@ -0,0 +1,37 @@
+﻿<?php
+/**
+ *
+ *	Kirino Minecraft Server Manager
+ *
+ *	Powered by Kirinovo
+ *
+ *	https://www.kirino.cn/
+ *
+ *	module name: Kirinos
+ *
+ *	module description: Kirinos Main class
+ *
+ **/
+ 
+class Kirinos {
+	
+	public function Error ($exceptionType) {
+		
+		switch ($exceptionType) {
+			case 0:
+				echo "<b>Kirinos Error: </b>Class file not found.";
+				break;
+			case 400:
+				echo "<b>Kirinos Error: </b>400 Bad request.";
+				break;
+		}
+		
+	}
+	
+	public function Loc302 ($url) {
+		
+		echo "<script>location='" . $url . "';</script>";
+		exit;
+		
+	}
+}
