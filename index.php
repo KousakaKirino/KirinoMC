7ef833d036b0ceac75e5deeb2cc66e727430b103
Prismaillya <31351878+Prisma-illya@users.noreply.github.com>
2017-10-28 22:25:06 +0800

    KirinoMC 主程序

---
+++ index.php
@@ -0,0 +1,15 @@
+﻿<?php
+/**
+ *
+ *	Kirino Minecraft Server Manager
+ *
+ *	Powered by Kirinovo
+ *
+ *	https://www.kirino.cn/
+ *
+ **/
+ 
+define("ROOTDIR", str_replace("\\", "/", __DIR__));
+include(ROOTDIR . "/kirinos/app/loader.php");
+$Loader = new Loader();
+$Loader->start();
