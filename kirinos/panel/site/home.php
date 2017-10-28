c415b13dc62031e5c3d9d32335cd19d09f929d52
KousakaKirino <Kirino@tcotp.cn>
2017-10-28 22:31:57 +0800

    1

--- kirinos/panel/site/home.php
+++ kirinos/panel/site/home.php
@@ -1,18 +1,2 @@
-﻿<?php
-OB_START();
-SESSION_START();
-$descriptorspec = array( 
-    0 => array("pipe", "r"), 
-    1 => array("pipe", "w")
-); 
-/*$handle = proc_close(proc_open(
-    'cd E:\mctest&java -jar PaperSpigot-1.8.8-R0.1-SNAPSHOT.jar',
-    $descriptorspec,
-    $pipes
-));*/
-echo "aaaaaaaa";
-ob_flush();
-ob_clean();
-$handle = proc_open('cd E:\mctest&java -jar PaperSpigot-1.8.8-R0.1-SNAPSHOT.jar', $descriptorspec, $test);
-echo iconv("GB2312", "UTF-8", fgets($test[1]));
-proc_close($handle);
+<?php
+echo RequestId;
